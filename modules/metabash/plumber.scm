5759a7f2a17f9199d7e324d44c8bc1df5cc34931
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2020-08-05 17:51:25 +0300

    plumber: Improve
    * modules/metabash/plumber.scm: Improve.
      (run-local, run-remote, append-process): New procedures.
      (plumb): Use it.

--- modules/metabash/plumber.scm
+++ modules/metabash/plumber.scm
@@ -2,6 +2,7 @@
   #:use-module (metabash pipe)
   #:use-module (metabash process)
   #:use-module (srfi srfi-9 gnu)
+  #:use-module (srfi srfi-11)
   #:export (<pipeline>
             pipeline?
             pipeline-pipes
@@ -45,38 +46,40 @@
 
 
 
+(define (run-local command)
+  (make-process #f (cadr command)))
+
+(define (run-remote command)
+  (let ((host (cadr  command))
+        (cmd  (caddr command)))
+    (make-process host cmd)))
+
+(define (append-process process-list pipe-list process)
+  (let ((last-proc (last process-list)))
+    (if last-proc
+        (values (append-1 process-list process)
+                (append-1 pipe-list    (make-pipe (process-output-port last-proc)
+                                                  (process-input-port  process))))
+        (values (append-1 process-list process)
+                pipe-list))))
+
 (define (plumb . spec)
   "Make a pipeline using the SPEC."
   (let loop ((sp        spec)
              (pipes     '())
              (processes '()))
     (if (not (null? sp))
-        (let* ((command   (car sp))
-               (type      (car command))
-               (last-proc (last processes)))
+        (let* ((command (car sp))
+               (type    (car command)))
           (case type
             ((local)
-             (let ((proc (make-process #f (cadr command))))
-               (loop (cdr sp)
-                     (if last-proc
-                         (append-1 pipes
-                                   (make-pipe
-                                    (process-output-port last-proc)
-                                    (process-input-port  proc)))
-                         pipes)
-                     (append-1 processes proc))))
+             (let-values (((process-list pipe-list)
+                           (append-process processes pipes (run-local command))))
+               (loop (cdr sp) process-list pipe-list)))
             ((remote)
-             (let* ((host (cadr  command))
-                    (cmd  (caddr command))
-                    (proc (make-process host cmd)))
-               (loop (cdr sp)
-                     (if last-proc
-                         (append-1 pipes
-                                   (make-pipe
-                                    (process-output-port last-proc)
-                                    (process-input-port  proc)))
-                         pipes)
-                     (append-1 processes proc))))))
+             (let-values (((process-list pipe-list)
+                          (append-process processes pipes (run-remote command))))
+               (loop (cdr sp) process-list pipe-list)))))
         (make-pipeline pipes processes))))
 
 ;;; plumber.scm ends here.
