b200e922c2f06b3774c6415253346851e6c87d68
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2020-08-06 23:22:21 +0300

    plumber.scm, process.scm: Add module commentary
    * modules/metabash/plumber.scm, modules/metabash/process.scm: Add module
      commentary.

--- modules/metabash/process.scm
+++ modules/metabash/process.scm
@@ -1,3 +1,28 @@
+;;; process.scm -- Metabash process implementation.
+
+;; Copyright (C) 2020 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This file contains implementation of Metabash processes.
+
+
+;;; Code:
+
 (define-module (metabash process)
   #:use-module (srfi srfi-9 gnu)
   #:use-module (ice-9 popen)
@@ -64,3 +89,4 @@
       (with-ssh (make-node host)
                 (delete-file (process-fifo-name process)))))))
 
+;;; process.scm ends here.
